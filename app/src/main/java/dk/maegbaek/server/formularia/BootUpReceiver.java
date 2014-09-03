4606e384d3a7d7b39d794a40a65f600b6c5136bc
olelundmaegbaek <ole@maegbaek.dk>
2014-09-03 13:34:29 +0200

    Sat op til launcher. Kan ændres under administration.

--- app/src/main/java/dk/maegbaek/server/formularia/BootUpReceiver.java
+++ app/src/main/java/dk/maegbaek/server/formularia/BootUpReceiver.java
@@ -1,21 +1,18 @@
-package dk.maegbaek.server.formularia;
+//package dk.maegbaek.server.formularia;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
+//import android.content.BroadcastReceiver;
+//import android.content.Context;
+//import android.content.Intent;
 
-/**
- * Created by Ole on 19-08-2014.
- */
-public class BootUpReceiver extends BroadcastReceiver {
+//public class BootUpReceiver extends BroadcastReceiver {
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (Settings.isOnBoot(context)) {
-            Intent i = new Intent(context, WebFormularia.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(i);
-        }
-    }
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
+//        if (Settings.isOnBoot(context)) {
+//            Intent i = new Intent(context, WebFormularia.class);
+//            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            context.startActivity(i);
+//        }
+//    }
 
-}
+//}
